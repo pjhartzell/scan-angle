4c6fa35da26f001d2e467f790ec20581956842b4
Preston's Dell Laptop <preston.hartzell@gmail.com>
2020-05-29 16:38:38 -0500

    throw out high angles

--- traj_sa.py
+++ traj_sa.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 from traj_sa_funcs import read_las, time_block_indices, traj_xyz_mean, save_traj
 
@@ -11,15 +12,18 @@ from traj_sa_funcs import read_las, time_block_indices, traj_xyz_mean, save_traj
 # pipeline in the read_las function if desired.
 
 # USER INPUT
-filename = "F:/UH/C2_L2_sorted.las"
+# filename = "F:/UH/C2_L2_sorted.las"
+filename = "F:/Sitka/helipod/autoclass - Scanner 1 - 160503_011252_VQ480i - originalpoints_timesorted.las"
 delta_t = 0.1       # Time block duration (seconds)
 min_delta_a = 15    # Minimum scan angle range within a time block (degrees)
-num_ests = 6400      # Number of trajectory estimates to average in a time block
+num_ests = 100      # Number of trajectory estimates to average in a time block
 # ------------------------------------------------------------------------------
 
 
 # Get time, x, y, z, and scan angle rank from timesorted LAS file
 txyza = read_las(filename)
+
+start_time = time.time()
 
 # Time block start locations
 indices = time_block_indices(txyza[:,0], delta_t)
@@ -31,11 +35,18 @@ for idx1, idx2 in zip(indices[:-1], indices[1:]):
 
     a = txyza[idx1:idx2,4]
 
+    # Max and min scan angles introduce uncompensated systematic error
+    a_max = np.max(a) - 1
+    a_min = np.min(a) + 1
+
     # Check for sufficient geometry
-    if (np.max(a) - np.min(a)) >= min_delta_a:
+    if (a_max - a_min) >= min_delta_a:
+
+        keep = np.logical_and(a >= a_min, a <= a_max)
+        a = a[keep]
 
         # Check for sufficient number of points
-        if a.shape[0] > (2*num_ests):
+        if len(a) > (2*num_ests):
 
             sort_idx = np.argsort(a)
             low_idx = sort_idx[0:num_ests]
@@ -48,16 +59,36 @@ for idx1, idx2 in zip(indices[:-1], indices[1:]):
             if not np.isin(a_low, a_high).any():  
 
                 xyz = txyza[idx1:idx2,1:4]
+                xyz = xyz[keep,:]
+                xyz_low = xyz[low_idx,:]
+                xyz_high = xyz[high_idx,:]
+
                 t = txyza[idx1:idx2,0]
+                t = t[keep]
+                t_low = t[low_idx]
+                t_high = t[high_idx]
+
+                # randomize the data
+                p_low = np.random.permutation(num_ests)
+                p_high = np.random.permutation(num_ests)
+                a_low = a_low[p_low]
+                a_high = a_high[p_high]
+                xyz_low = xyz_low[p_low,:]
+                xyz_high = xyz_high[p_high,:]
+                t_low = t_low[p_low]
+                t_high = t_high[p_high]
 
                 x_mean, y_mean, z_mean = traj_xyz_mean(
-                    xyz[low_idx,:],
-                    xyz[high_idx,:],
+                    xyz_low,
+                    xyz_high,
                     a_low,
                     a_high)
-                t_mean = np.mean(t[np.hstack((low_idx, high_idx))])
+                t_mean = np.mean(np.hstack((t_low, t_high)))
 
                 traj_txyz.append([t_mean, x_mean, y_mean, z_mean])
+
+end_time = time.time()
+print("Time = {}".format(end_time-start_time))
 
 save_traj(filename, traj_txyz)
 
