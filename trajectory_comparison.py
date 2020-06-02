b7002b72d53b769f689bf842c2bcc57c67299bc6
Preston Hartzell <preston.hartzell@gmail.com>
2020-06-02 08:45:52 -0500

    sitka

--- trajectory_comparison.py
+++ trajectory_comparison.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-actual_traj_file = 'F:/UH/sbet_047_IGS08-UTM15N-Ellipsoid.txt'
-est_traj_file = 'F:/UH/est_6400.txt'
+actual_traj_file = 'G:/Sitka/helipod/apps_final_ATLANS-20160503_NAD83-UTM8N-Geoid12B.txt'
+est_traj_file = 'G:/Sitka/helipod/orig400.txt'
 
 
 actual_traj = np.loadtxt(actual_traj_file, delimiter=',', skiprows=1)
